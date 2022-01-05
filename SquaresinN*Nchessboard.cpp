01513e900b410955907e062faca25233af1b29c8
vpavankalyan <bcs_2019069@iiitm.ac.in>
2022-01-05 19:01:10 +0530

    Added C1-Q12

---
+++ SquaresinN*Nchessboard.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        if(N==1){
+            return 1;
+        }
+        return N*(N+1)*((2*N)+1)/6;
+    }
+};
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  
